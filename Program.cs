0f373841fa3d89aab2142ce9b2adab51f4a14b63
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-08-30 16:59:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new List<int>()
+            { 1, 2, 3, 4, 5};
+
+        numbers.Reverse();
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
